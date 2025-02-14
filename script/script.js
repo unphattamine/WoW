31825d40e848c9a2b9af52d055761c7622fb4589
Samuel Imrich <SImrich@bell-integration.com>
2025-02-14 11:15:37 +0100

    fixed

--- script/script.js
+++ script/script.js
@@ -1,16 +1,14 @@
 //CALCULATES SOW PRICE
 function calculatePrice() {
+
+    //CONTACT INFO
     let fcustomer = document.getElementById('fcustomer').value;
     let fname = document.getElementById('fname').value;
     let fmail = document.getElementById('fmail').value;
-    console.log(fcustomer+fname+fmail)
+    
+    //MAIN SCOPE
     let complexUCs = parseInt(document.getElementById("complexUCs").value) || 0;
     let simpleUCs = parseInt(document.getElementById("simpleUCs").value) || 0;
-    let hypercare = parseInt(document.getElementById("Hypercare").value) || 0;
-    let UAT = parseInt(document.getElementById("UAT").value) || 0;
-
-    //INTEGRATIONS
-    let integrations = document.querySelector('input[name="integrationRequirements"]:checked') ? document.querySelector('input[name="integrationRequirements"]:checked').value : 'No option selected';
     //CHANNELS
     let channels = document.querySelectorAll('#channel input[type="checkbox"]');
     let checkedChannels = [];
@@ -20,10 +18,59 @@ function calculatePrice() {
         }
     });
     channels = checkedChannels
+    //LANGUAGE
+    let productLanguage = document.querySelector('input[name="productLanguage"]:checked');
+    //AMELIA TRAINING
+    let AmeliaTrainingCourse = document.querySelector('input[name="AmeliaTrainingCourse"]:checked');
+    //NOTES
+    let fnotes = document.getElementById('fnotes').value;
 
+    //DELIVERY TEAM LOCATION
+    let customTimezone = document.querySelector('input[name="customTimezone"]:checked');
+    let customLocation = document.querySelector('input[name="customLocation"]:checked');
     
-    let enablement = parseInt(document.getElementById("Enablement").value) || 0;
+    //ADDITIONAL TIMELINE REQS
+    let Hypercare = parseInt(document.getElementById("Hypercare").value) || 0;
+    let UAT = parseInt(document.getElementById("UAT").value) || 0;
+    let Enablement = parseInt(document.getElementById("Enablement").value) || 0;
+    let GoLiveApproach = document.querySelector('input[name="GoLiveApproach"]:checked');
+
+    //INTEGRATIONS
+    let integrationRequirements = document.querySelector('input[name="integrationRequirements"]:checked');
+    let APInumbers = parseInt(document.getElementById("APInumbers").value) || 0;
+    let APIStartDate = document.querySelector('input[name="APIStartDate"]:checked');
+    let APIExposed = document.querySelector('input[name="APIExposed"]:checked');
+    let ISCRequirements = document.querySelector('input[name="ISCRequirements"]:checked');
+    let fAPInotes = document.getElementById('fAPInotes').value;
+
+    //NLU
+    let IntentModelRequired = document.querySelector('input[name="IntentModelRequired"]:checked');
+    let IntentNumber = parseInt(document.getElementById("IntentNumber").value) || 0;
+    let contextswitching = document.querySelector('input[name="contextswitching"]:checked');
+    let customLeveling = document.querySelector('input[name="customLeveling"]:checked');
+    let EntityRequirements = document.querySelector('input[name="EntityRequirements"]:checked');
+    let IntentModelTrainingData = document.querySelector('input[name="IntentModelTrainingData"]:checked');
+
+    //PROJECT MGMT
+    let EndCustomerGovernanceRequired = document.querySelector('input[name="EndCustomerGovernanceRequired"]:checked');
+    let AmeliaCoordination = document.querySelector('input[name="AmeliaCoordination"]:checked');
+
+    //VOICE DAVANCED
+    let DTMFRequired = document.querySelector('input[name="DTMFRequired"]:checked');
+    let BargeInRequired = document.querySelector('input[name="BargeInRequired"]:checked');
+
+    //OTHERS
     let customReporting = document.querySelector('input[name="customReporting"]:checked') ? document.querySelector('input[name="customReporting"]:checked').value : 'No option selected';
+    let ContentSharingRequired = document.querySelector('input[name="ContentSharingRequired"]:checked');
+    let RecordingsRequired = document.querySelector('input[name="RecordingsRequired"]:checked');
+    let DynamicFAQTraining = document.querySelector('input[name="DynamicFAQTraining"]:checked');
+    let DataInformationMasking = document.querySelector('input[name="DataInformationMasking"]:checked');
+    let DemoRequirements = document.querySelector('input[name="DemoRequirements"]:checked');
+
+
+
+    let enablement = parseInt(document.getElementById("Enablement").value) || 0;
+    
     let multiplier = (channel === "Voice" || channel === "Chat") ? 1 : 1.7;
 
     let solutionTimeline = (((complexUCs * config.complexUCsMultiplier) + (simpleUCs * config.simpleUCsMultiplier)) * multiplier) + config.baseSolutionTimeline;
@@ -31,30 +78,78 @@ function calculatePrice() {
     let cost = (config.costPerHour * 5 * config.solutionMultiplier * solutionTimeline) + 
                (config.devCostPerHour * 5 * devTimeline) + 
                (config.costPerHour * 5 * config.solutionAdditionalMultiplier * (devTimeline + solutionTimeline)) + 
-               (config.devCostPerHour * 5 * (hypercare + UAT)) + 
+               (config.devCostPerHour * 5 * (Hypercare + UAT)) + 
                (enablement * config.enablementCost);
     let sell = (config.sellPerHour * 5 * config.solutionMultiplier * solutionTimeline) + 
                (config.devSellPerHour * 5 * devTimeline) + 
                (config.sellPerHour * 5 * config.sellAdditionalMultiplier * (devTimeline + solutionTimeline)) + 
-               (config.devSellPerHour * 5 * (hypercare + UAT)) + 
+               (config.devSellPerHour * 5 * (Hypercare + UAT)) + 
                (enablement * config.enablementSell);
+
+
+    localStorage.setItem("fcustomer", fcustomer);
+    localStorage.setItem("fmail", fmail);
+    localStorage.setItem("fname", fname);
+
+    localStorage.setItem("complexUCs", complexUCs);
+    localStorage.setItem("simpleUCs", simpleUCs);
+    localStorage.setItem("channels", channels);
+    localStorage.setItem("productLanguage", productLanguage.value);
+    localStorage.setItem("AmeliaTrainingCourse", AmeliaTrainingCourse.value);
+    localStorage.setItem("fnotes", fnotes);
+
+    localStorage.setItem("customTimezone", customTimezone.value);
+    localStorage.setItem("customLocation", customLocation.value);
+    localStorage.setItem("Hypercare", Hypercare);
+    localStorage.setItem("UAT", UAT);
+    localStorage.setItem("Enablement", Enablement);
+    localStorage.setItem("GoLiveApproach", GoLiveApproach.value);
+
+    localStorage.setItem("integrationRequirements", integrationRequirements.value);
+    localStorage.setItem("APInumbers", APInumbers);
+    localStorage.setItem("APIStartDate", APIStartDate.value);
+    localStorage.setItem("APIExposed", APIExposed.value);
+    localStorage.setItem("ISCRequirements", ISCRequirements.value);
+    localStorage.setItem("fAPInotes", fAPInotes);
+
+    localStorage.setItem("IntentModelRequired", IntentModelRequired.value);
+    localStorage.setItem("IntentNumber", IntentNumber);
+    localStorage.setItem("contextswitching", contextswitching.value);
+    localStorage.setItem("customLeveling", customLeveling.value);
+    localStorage.setItem("EntityRequirements", EntityRequirements.value);
+    localStorage.setItem("IntentModelTrainingData", IntentModelTrainingData.value);
+
+    localStorage.setItem("EndCustomerGovernanceRequired", EndCustomerGovernanceRequired.value);
+    localStorage.setItem("AmeliaCoordination", AmeliaCoordination.value);
+
+    localStorage.setItem("DTMFRequired", DTMFRequired.value);
+    localStorage.setItem("BargeInRequired", BargeInRequired.value);
+
+    localStorage.setItem("customReporting", customReporting.value);
+    localStorage.setItem("ContentSharingRequired", ContentSharingRequired.value);
+    localStorage.setItem("RecordingsRequired", RecordingsRequired.value);
+    localStorage.setItem("DynamicFAQTraining", DynamicFAQTraining.value);
+    localStorage.setItem("DataInformationMasking", DataInformationMasking.value);
+    localStorage.setItem("DemoRequirements", DemoRequirements.value);
 
     localStorage.setItem("solutionTimeline", solutionTimeline.toFixed(2));
     localStorage.setItem("devTimeline", devTimeline.toFixed(2));
     localStorage.setItem("cost", cost.toFixed(2));
     localStorage.setItem("sell", sell.toFixed(2));
     localStorage.setItem("fcustomer", fcustomer);
+    localStorage.setItem("fnotes", fnotes);
     localStorage.setItem("fmail", fmail);
     localStorage.setItem("fname", fname);
-    localStorage.setItem("channels", channels);
-    localStorage.setItem("integrations", integrations);
+    
+    //localStorage.setItem("integrations", integrations);
+ 
 
     localStorage.setItem("customReporting", customReporting);
     customReporting = localStorage.getItem("customReporting");
     if (customReporting) {
-        console.log(`Loaded Custom Reporting Value: ${customReporting}`);
+        //console.log(`Loaded Custom Reporting Value: ${customReporting}`);
     } else {
-        console.log('No custom reporting value found in local storage.');
+        //console.log('No custom reporting value found in local storage.');
     }
 
 /*
--- script/script_results.js
+++ script/script_results.js
@@ -1,25 +1,79 @@
 window.onload = function() {
+
     document.getElementById("solutionTimeline").innerText = localStorage.getItem("solutionTimeline") + " weeks";
     document.getElementById("devTimeline").innerText = localStorage.getItem("devTimeline") + " weeks";
     document.getElementById("cost").innerText = "GBP " + localStorage.getItem("cost");
     document.getElementById("sell").innerText = "GBP " + localStorage.getItem("sell");
     
+
+    
+    //DOCUMENT DATE
+    document.getElementById('documentDate').textContent = new Date().toLocaleDateString('en-GB');
+
+    //NOTES
+    document.getElementById("fnotes").innerText = localStorage.getItem("fnotes");
+    
     //CUSTOMER
+    //getting all elements with the id fcustomer, not recommended really
     document.getElementById("fcustomer").innerText = localStorage.getItem("fcustomer");
+    document.getElementById("fcustomer1").innerText = localStorage.getItem("fcustomer");
     document.getElementById("fname").innerText = localStorage.getItem("fname");
     document.getElementById("fmail").innerText = localStorage.getItem("fmail");
     
+    document.getElementById("complexUCs").innerText = localStorage.getItem("complexUCs");
+    document.getElementById("simpleUCs").innerText = localStorage.getItem("simpleUCs");
+    const channels = localStorage.getItem("channels");
+    const productLanguage = localStorage.getItem("productLanguage");
+    document.getElementById("AmeliaTrainingCourse").innerText = localStorage.getItem("AmeliaTrainingCourse");
+    document.getElementById("customTimezone").innerText = localStorage.getItem("customTimezone");
+    document.getElementById("customLocation").innerText = localStorage.getItem("customLocation");
+
+    document.getElementById("Hypercare").innerText = localStorage.getItem("Hypercare");
+    document.getElementById("UAT").innerText = localStorage.getItem("UAT");
+    document.getElementById("Enablement").innerText = localStorage.getItem("Enablement");
+    document.getElementById("GoLiveApproach").innerText = localStorage.getItem("GoLiveApproach");
+    
+    document.getElementById("integrationRequirements").innerText = localStorage.getItem("integrationRequirements");
+    document.getElementById("APInumbers").innerText = localStorage.getItem("APInumbers");
+    document.getElementById("APIStartDate").innerText = localStorage.getItem("APIStartDate");
+    document.getElementById("APIExposed").innerText = localStorage.getItem("APIExposed");
+    document.getElementById("ISCRequirements").innerText = localStorage.getItem("ISCRequirements");
+    document.getElementById("fAPInotes").innerText = localStorage.getItem("fAPInotes");
+
+    document.getElementById("IntentModelRequired").innerText = localStorage.getItem("IntentModelRequired");
+    document.getElementById("IntentNumber").innerText = localStorage.getItem("IntentNumber");
+    document.getElementById("contextswitching").innerText = localStorage.getItem("contextswitching");
+    document.getElementById("customLeveling").innerText = localStorage.getItem("customLeveling");
+    document.getElementById("EntityRequirements").innerText = localStorage.getItem("EntityRequirements");
+    document.getElementById("IntentModelTrainingData").innerText = localStorage.getItem("IntentModelTrainingData");
+
+    document.getElementById("EndCustomerGovernanceRequired").innerText = localStorage.getItem("EndCustomerGovernanceRequired");
+    document.getElementById("AmeliaCoordination").innerText = localStorage.getItem("AmeliaCoordination");
+
+    document.getElementById("DTMFRequired").innerText = localStorage.getItem("DTMFRequired");
+    document.getElementById("BargeInRequired").innerText = localStorage.getItem("BargeInRequired");
+
+    document.getElementById("customReporting").innerText = localStorage.getItem("customReporting");
+    document.getElementById("ContentSharingRequired").innerText = localStorage.getItem("ContentSharingRequired");
+    document.getElementById("RecordingsRequired").innerText = localStorage.getItem("RecordingsRequired");
+    document.getElementById("DynamicFAQTraining").innerText = localStorage.getItem("DynamicFAQTraining");
+    document.getElementById("DataInformationMasking").innerText = localStorage.getItem("DataInformationMasking");
+    document.getElementById("DemoRequirements").innerText = localStorage.getItem("DemoRequirements");
 
     const integrations = localStorage.getItem("integrations");
-    const channels = localStorage.getItem("channels");
+    
+    //console.log(productLanguage);
+    document.getElementById("channels").innerText = localStorage.getItem("channels");
+    document.getElementById("productLanguage").innerText = localStorage.getItem("productLanguage");
 
-    //console.log(channels)
+
+    //REPORTING
     const customReporting = localStorage.getItem("customReporting");
-    console.log(customReporting)
+    //console.log(customReporting)
     if (customReporting) {
-        console.log(`Loaded Custom Reporting Value: ${customReporting}`);
+        //console.log(`Loaded Custom Reporting Value: ${customReporting}`);
     } else {
-        console.log('No custom reporting value found in local storage.');
+        //console.log('No custom reporting value found in local storage.');
     }
 
     //REPORTING WARNING
@@ -32,22 +86,22 @@ window.onload = function() {
 
     //IF CHANNEL EMAIL & NO INTEGRATIONS
     if(integrations == "false" && channels.includes('email')){
-        console.log("channels verified - FAIL");
+        //console.log("channels verified - FAIL");
         let message = document.createElement("p");
         message.innerText = "⛔ We can not deliver email channel without email or API integration. Please discuss the implementation possibilities with technical team";
         message.classList.add("message");
         document.getElementById("noIntegrations").appendChild(message);
     } else {
-        console.log("channels verified - OK");
+        //console.log("channels verified - OK");
     }
     if(integrations == "false" && channels.includes('SMS')) {
-        console.log("channels verified - FAIL");
+        //console.log("channels verified - FAIL");
         let message = document.createElement("p");
         message.innerText = "⛔ We can not deliver SMS channel without SMS or API integration. Please discuss the implementation possibilities with technical team";
         message.classList.add("message");
         document.getElementById("noIntegrations").appendChild(message);
     } else {
-        console.log("channels verified - OK");
+        //console.log("channels verified - OK");
     }
 };
 
@@ -124,3 +178,56 @@ function downloadPDF() {
         });
 }
 
+async function exportHTMLToDocx() {
+    const cssResponse = await fetch('style/style.css');
+    const cssContent = await cssResponse.text();
+    const htmlContent = document.getElementById("pdf_results").outerHTML;
+
+    // Create a Word document structure with embedded CSS
+    const preHtml = `
+    <html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'>
+    <head><meta charset='utf-8'><title>Export HTML to DOC</title>
+    <style>
+        ${cssContent}
+    </style>
+    </head><body>`;
+const postHtml = `</body></html>`;
+const fullHtml = preHtml + htmlContent + postHtml;
+
+// Create a blob with the Word document content
+const blob = new Blob(['\ufeff', fullHtml], {
+    type: 'application/msword'
+});
+
+// Create a link element
+const link = document.createElement("a");
+link.href = URL.createObjectURL(blob);
+link.download = "SOW_draft.doc";
+
+// Append the link to the body
+document.body.appendChild(link);
+
+// Programmatically click the link to trigger the download
+link.click();
+
+// Remove the link from the document
+document.body.removeChild(link);
+  }
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    function updateSpans() {
+        const spans = document.querySelectorAll("#tableSummary span");
+
+        spans.forEach(span => {
+            const text = span.textContent.toLowerCase();
+            if (text === "no" || text === "false") {
+                span.textContent = "❌";
+            } else if (text === "yes" || text === "true") {
+                span.textContent = "✔️";
+            }
+        });
+    }
+    updateSpans();
+    setInterval(updateSpans, 100);
+});
